865351848c2bc4adc0b27f270a203dc51c2a1cb3
Alena <lenych_94@mail.ru>
2016-05-04 23:22:51 +0700

    Инициализация матрицы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.radioButtonUser = new System.Windows.Forms.RadioButton();
+            this.radioButtonComp = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(353, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Первый ход:";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(341, 281);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(126, 36);
+            this.buttonClear.TabIndex = 17;
+            this.buttonClear.Text = "Очистить поле";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonUser
+            // 
+            this.radioButtonUser.AutoSize = true;
+            this.radioButtonUser.Checked = true;
+            this.radioButtonUser.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonUser.Location = new System.Drawing.Point(341, 138);
+            this.radioButtonUser.Name = "radioButtonUser";
+            this.radioButtonUser.Size = new System.Drawing.Size(109, 22);
+            this.radioButtonUser.TabIndex = 16;
+            this.radioButtonUser.TabStop = true;
+            this.radioButtonUser.Text = "Пользователь";
+            this.radioButtonUser.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonComp
+            // 
+            this.radioButtonComp.AutoSize = true;
+            this.radioButtonComp.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonComp.Location = new System.Drawing.Point(341, 106);
+            this.radioButtonComp.Name = "radioButtonComp";
+            this.radioButtonComp.Size = new System.Drawing.Size(96, 22);
+            this.radioButtonComp.TabIndex = 15;
+            this.radioButtonComp.Text = "Компьютер";
+            this.radioButtonComp.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(15, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(302, 302);
+            this.panel1.TabIndex = 14;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(341, 241);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(126, 34);
+            this.buttonStart.TabIndex = 13;
+            this.buttonStart.Text = "Начать игру";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(505, 334);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.radioButtonUser);
+            this.Controls.Add(this.radioButtonComp);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Крестики - нолики";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.RadioButton radioButtonUser;
+        private System.Windows.Forms.RadioButton radioButtonComp;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_tdd
+{
+    public class Game
+    {
+        public Game()
+        {
+            matr = new int[3][];
+            for (int i = 0; i < 3; i++)
+                matr[i] = new int[3];
+        }
+
+        private int[][] matr;
+       
+
+        public void initMatr()
+        {
+           
+        }
+
+        public int[][] getMatr()
+        {
+            return matr;
+        }
+    }
+}
