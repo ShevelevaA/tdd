c37e2b2b4816ae906e46cda0fec7d7777dd5902f
Alena <lenych_94@mail.ru>
2016-05-05 00:00:41 +0700

    Тест на проверку первого игрока

--- Game.cs
+++ Game.cs
@@ -8,26 +8,33 @@ namespace test_tdd
 {
     public class Game
     {
+        private int[][] matr;
+        private int whoFirst;
+
         public Game()
         {
             matr = new int[3][];
             for (int i = 0; i < 3; i++)
                 matr[i] = new int[3];
-        }
 
-        private int[][] matr;
-       
+            whoFirst = 2; // первый ходит пользователь
+        }
 
         public void initMatr()
         {
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
                     matr[i][j] = 0;
+
         }
 
         public int[][] getMatr()
         {
             return matr;
+        }
+
+        public int getWhoFirst() {
+            return whoFirst;
         }
     }
 }
