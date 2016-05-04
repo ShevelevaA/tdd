e79d456d1e81af5cd587afc1c53b9f3aeda2bd4a
Alena <lenych_94@mail.ru>
2016-05-04 23:47:57 +0700

    Напишим хороший тест проверки инициализации матрицы

--- Game.cs
+++ Game.cs
@@ -22,9 +22,8 @@ namespace test_tdd
         {
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
-                    matr[i][0] = 0;
-            matr[3][3] = 1;
-
+                    matr[i][j] = 0;
+            matr[2][2] = 1;
         }
 
         public int[][] getMatr()
